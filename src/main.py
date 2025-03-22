254417bb334c221155daaccc07ad0104eec45d26
wilsyn-git <orders@siraim.com>
2025-03-22 14:31:10 -0400

    adding my python. i'm sure i got my textnode wrong

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import textnode
+
+def main():
+    basic_thing = textnode.TextNode("this is some anchor text", "stuff", "Some")
+
+    print("hello world")
+    print(basic_thing)
+
+if __name__ == "__main__":
+    main()
+
---
+++ src/textnode.py
@@ -0,0 +1,11 @@
+class TextNode:
+    def __init__(self, text, text_type, url = "None"):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other_node):
+        return self == other_node
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
