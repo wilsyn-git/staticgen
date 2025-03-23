460089c0dacff71c41629ad3822dc0fc5c2558da
wilsyn-git <orders@siraim.com>
2025-03-22 21:31:09 -0400

    l4 adding markdown detector

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,5 +1,8 @@
 
 from textnode import TextNode, TextType
+import re
+
+
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -21,3 +24,16 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 split_nodes.append(TextNode(sections[i], text_type))
         new_nodes.extend(split_nodes)
     return new_nodes
+
+
+def extract_markdown_images(text):
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text) 
+    return matches
+
+def extract_markdown_links(text):
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text) 
+    return matches
+
+
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,7 +1,11 @@
 
 import unittest
 from inline_markdown import (
+    extract_markdown_images,
+    extract_markdown_links,
     split_nodes_delimiter,
+    extract_markdown_links,
+    extract_markdown_images,
 )
 
 from textnode import TextNode, TextType
@@ -86,6 +90,26 @@ class TestInlineMarkdown(unittest.TestCase):
             ],
             new_nodes,
         )
+
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev)"
+        )
+        self.assertListEqual(
+            [
+                ("link", "https://boot.dev"),
+                ("another link", "https://blog.boot.dev"),
+            ],
+            matches,
+        )
+
 
 
 if __name__ == "__main__":
